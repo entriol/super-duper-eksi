181008a3645eb8ca8363dcb393ba602050bef55b
entriol <entriol@outlook.com>
2016-03-09 16:47:57 +0200

    mesajlaşma arabiriminde bkz, hede... butonları.

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Ekşi Sözlük'teki eksik özellikleri tamamlayan bir eklenti.
 
+Şimdilik spagetti ve bolca jQuery dolu olsa da öncelikle birkaç özelliği tamamlayıp sonrasında öğrendikçe daha temiz şekle dönüştüreceğim. Katkılarınız, görüşleriniz olursa memnuniyetle karşılarım.
+
 **29 Şub 16**
 - Otomatik olarak entryleri uzun olarak açar. (bkz: [devamını okuyayım](https://eksisozluk.com/devamini-okuyayim--3762889))
 
@@ -8,5 +10,14 @@ Ekşi Sözlük'teki eksik özellikleri tamamlayan bir eklenti.
 - Bir yazarın başlıkta yazdığı entryleri hızlıca görebilmek için kısayol bağlantısı.
 - Eğer başlık aynı zamanda bir sözlük yazarına aitse, sağ sütunda yazar profiline bağlantı.
 
+**8 Mar 16**
+- Mesajlaşma arabiriminde bkz, hede, link gibi düğmeler.
+
+
+###### Bilinen hatalar
+- Yazdıklarınız formu aşarsa, mesajlaşmada bkz, hede butonları kullanıldığında scroll en sona zıplıyor,
+ama imleç doğru yerde kalıyor. Şimdilik çözüm bulamadım. Gerçi, bu sorun sözlüğün kendisinde de var.
+Entry yazarken düğmeleri kullandığınızda, imleçi en sona atılıyor.
+
+
 [Chrome Store](https://chrome.google.com/webstore/detail/cghpmfaamkmfolaalbfjnjilknkhiacn)
-Chrome mağazasındaki sürüm muhtemelen oldukça geriden takip ederek güncellenecektir.
--- content.js
+++ content.js
@@ -3,54 +3,54 @@
 // FEATURE: Auto-read-more.
 var readMores = document.querySelectorAll("span.read-more-link-wrapper a");
 Array.prototype.forEach.call(readMores, function(el, i){
-  el.click();
+    el.click();
 });
 
 // FEATURE: When adding it to trolls, note entry id to author profile.
 function initTrollSequence(author, entryLink) {
-  $.ajax({
-    type: "GET",
-    url: "https://eksisozluk.com/biri/"+ author,
-    success: function(data){
-        let toGetEntryId = entryLink.lastIndexOf("/");
-        let getEntryId = entryLink.substring(toGetEntryId + 1);
+    $.ajax({
+        type: "GET",
+        url: "https://eksisozluk.com/biri/"+ author,
+        success: function(data){
+            let toGetEntryId = entryLink.lastIndexOf("/");
+            let getEntryId = entryLink.substring(toGetEntryId + 1);
         
-        if ($(data).find('#user-note').first().val().indexOf("(bkz: #" + getEntryId + ")") >= 0){
-            console.log("Notlarıma bakıyorum... Evet, böyle bir notu daha önce almışız.")
+            if ($(data).find('#user-note').first().val().indexOf("(bkz: #" + getEntryId + ")") >= 0){
+                console.log("Notlarıma bakıyorum... Evet, böyle bir notu daha önce almışız.")
             return;
+            }
+            else {
+                postTrollNote(
+                    author,
+                    $(data).find('#user-note-form input').first().val(),
+                    $(data).find('#user-note').first().text(),
+                    "troll entrysi (bkz: #" + getEntryId + ")"
+                );
+            }
         }
-        else {
-        postTrollNote(
-            author,
-            $(data).find('#user-note-form input').first().val(),
-            $(data).find('#user-note').first().text(),
-            "troll entrysi (bkz: #" + getEntryId + ")"
-        );
-        }
-    },
-  });
+    });
 }
 
 function postTrollNote (author, who, oldNotes, trollEntry) {
-  $.ajax({
-    type: "POST",
-    url: "https://eksisozluk.com/biri/" + author + "/note",
-    data: { usernote: oldNotes + "\r\n"+trollEntry, who: who},
-    success: function(res){console.log("Not sure if trolling or stupid.")},
-    dataType: "text"
-  });
+    $.ajax({
+        type: "POST",
+        url: "https://eksisozluk.com/biri/" + author + "/note",
+        data: { usernote: oldNotes + "\r\n"+trollEntry, who: who},
+        success: function(res){console.log("Not sure if trolling or stupid.")},
+        dataType: "text"
+    });
 }
 
 Array.prototype.forEach.call(document.querySelectorAll("div.other ul.dropdown-menu li"), function(el, i){
-  if (el.textContent != "engelle") return;
-  else {
-    el.addEventListener("click", function(){
-        initTrollSequence(
-            this.parentNode.parentNode.parentNode.querySelectorAll('.entry-author')[0].textContent,
-            this.parentNode.parentNode.parentNode.querySelectorAll('.permalink')[0].href
+    if (el.textContent != "engelle") return;
+    else {
+        el.addEventListener("click", function(){
+            initTrollSequence(
+                this.parentNode.parentNode.parentNode.querySelectorAll('.entry-author')[0].textContent,
+                this.parentNode.parentNode.parentNode.querySelectorAll('.permalink')[0].href
             );
-    }, true);  
-  }
+        }, true);  
+    }
 });
 
 // FEATURE: All author entries about title.
@@ -70,3 +70,86 @@ $.ajax({
         }
     }
 });
+
+
+// FEATURE: Messaging tools.
+// Credit - fn.setCaretToPos: http://stackoverflow.com/questions/499126/jquery-set-cursor-position-in-text-area
+function setSelectionRange(input, selectionStart, selectionEnd) {
+    if (input.setSelectionRange) {
+        input.focus();
+        input.setSelectionRange(selectionStart, selectionEnd);
+    }
+    else if (input.createTextRange) {
+        var range = input.createTextRange();
+        range.collapse(true);
+        range.moveEnd('character', selectionEnd);
+        range.moveStart('character', selectionStart);
+        range.select();
+    }
+}
+
+function setCaretToPos (input, pos) {
+    setSelectionRange(input, pos, pos);
+}
+
+// Credit - fn.wrapText: http://stackoverflow.com/questions/32414659/wrap-only-selected-text-with-a-html-tag-using-javascript
+function wrapText(openTag, closeTag) {
+    var textArea = document.querySelectorAll('textarea[name=Message]')[0];    
+    var len = textArea.value.length;
+    var start = textArea.selectionStart;
+    var end = textArea.selectionEnd;
+    var selectedText = textArea.value.substring(start, end);
+    
+    var replacement = openTag + selectedText + closeTag;
+    textArea.value = textArea.value.substring(0, start) + replacement + textArea.value.substring(end, len);
+    
+    setCaretToPos(textArea, start + replacement.length);
+}
+
+if (window.location.pathname.indexOf('/mesaj/') > -1) {
+    var editTools = [
+        '<button type="button" title="bkz" tabindex="-1">(bkz: hede)</button>',
+        '<button type="button" title="hede" tabindex="-1">hede</button>',
+        '<button type="button" title="yildiz" tabindex="-1">*</button>',
+        '<button type="button" title="spoiler" tabindex="-1">-spoiler-</button>',
+        '<button type="button" title="http" tabindex="-1">http://</button>'
+    ]
+    
+    document.getElementById('cevapla').insertAdjacentHTML('afterend', editTools.join(" "));
+            
+    $('div#message-thread button').on('click', function(){
+        switch (this.getAttribute('title')) {
+            case 'bkz':
+                wrapText('(bkz: ', ')');
+                break;
+            case 'hede':
+                wrapText('`', '`');
+                break;
+            case 'yildiz':
+                wrapText('`:', '`');
+                break;
+            case 'spoiler':
+                wrapText('\n--- `spoiler` ---\n', '\n--- `spoiler` ---\n');
+                break;
+            case 'http':
+                var textArea = document.querySelectorAll('textarea[name=Message]')[0];
+                var start = textArea.selectionStart;
+                var end = textArea.selectionEnd;
+                
+                var link = prompt("Verilecek bağlantı adresi", "http://");
+                
+                if (start == end) {
+                    var linkTitle = prompt("Verilecek bağlantının adını giriniz", "");
+                    if (link != null && linkTitle != null) wrapText('['+link+ ' ', linkTitle + ']');                    
+                }
+                else {
+                    if (link != null) wrapText('['+link + ' ', ']');
+                }
+                break;
+        }
+    });
+}
+
+
+    
+    
